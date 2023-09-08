0ec22c70c8185c288b7b5dbfe48a6eaadec49f3c
raein11 <raein11@foxmail.com>
2023-09-08 23:25:43 +0800

    使用 sync.Mutex 实现并发读写

---
+++ geecache/cache.go
@@ -0,0 +1,35 @@
+package geecache
+
+import (
+	"geecache/lru"
+	"sync"
+)
+
+type cache struct {
+	mu         sync.Mutex
+	lru        *lru.Cache
+	cacheBytes int64
+}
+
+// 实现 Add 方法
+func (c *cache) Add(key string, value ByteView) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.lru == nil {
+		c.lru = lru.New(c.cacheBytes, nil)
+	}
+	c.lru.Add(key, value)
+}
+
+// 实现 Get 方法
+func (c *cache) Get(key string) (value ByteView, ok bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.lru == nil {
+		return
+	}
+	if v, ok := c.lru.Get(key); ok {
+		return v.(ByteView), ok
+	}
+	return
+}
