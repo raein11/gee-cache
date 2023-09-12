43532a63558a41879f44a39bb94126edd51d0f6a
raein11 <raein11@foxmail.com>
2023-09-12 21:38:34 +0800

    1.新增 Group 核心数据结构。2.新增构造函数 NewGroup

--- geecache/geecache.go
+++ geecache/geecache.go
@@ -1,5 +1,7 @@
 package geecache
 
+import "sync"
+
 // Getter 接口
 type Getter interface {
 	// Get 回调函数
@@ -13,4 +15,35 @@ type GetterFunc func(key string) ([]byte, error)
 // 方便使用者在调用时既能够传入函数作为参数， 也能够传入实现了该接口的结构体作为参数
 func (f GetterFunc) Get(key string) ([]byte, error) {
 	return f(key)
+}
+
+type Group struct {
+	name      string // 唯一的名称
+	getter    Getter // 缓存未命中时的回调
+	mainCache cache  // 并发缓存
+}
+
+// 全局变量
+var (
+	mu     sync.Mutex
+	groups = make(map[string]*Group)
+)
+
+// 构造函数，用于实例化 Group
+func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
+	// 1.判断是否传入回调函数
+	if getter == nil {
+		panic("nil Getter")
+	}
+	// 考虑到并发问题，先上锁
+	mu.Lock()
+	defer mu.Unlock() // 延迟释放：在 return 之后，函数返回结果之前
+	// 构造 Group
+	g := &Group{
+		name:      name,
+		getter:    getter,
+		mainCache: cache{cacheBytes: cacheBytes},
+	}
+	groups[name] = g
+	return g
 }
